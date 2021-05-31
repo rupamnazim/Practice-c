cf6defe41dc3caae08f3249be3fb9120092e2e93
Rupam Nazim <rupamnazim0@gmail.com>
2021-06-01 05:42:04 +0600

    As I write something

--- fao/fao.c
+++ fao/fao.c
@@ -3,7 +3,8 @@ int main()
 {
     int num[5] = {1,2,3,4,5};
     int i;
-    for(i=1;i<5;i++){
+    for(i=4;i<5;i=i+2){
     printf("These numbers are %d\n",num[i]);}
+    printf("%d",i);
     return 0;
 }
