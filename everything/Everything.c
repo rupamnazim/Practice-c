97e0b78086129faa34c367d0655a4d229f9acc79
Rupam Nazim <rupamnazim0@gmail.com>
2021-06-05 20:14:53 +0600

    great just commiting because I have to!

---
+++ everything/Everything.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+ int main(){
+
+        int x, y, '2';
+        x= y;
+        y= 2;
+        if(x=2){
+            y=3;
+            printf("%d",y);
+        }
+        if(x=2){
+            y=x;
+            x=2;
+            printf("%d",y);
+
+        }
+
+ return 0;
+ }
+
